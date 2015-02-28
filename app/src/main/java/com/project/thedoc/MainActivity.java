e39a2cb4991528b94e2d8f66d7bd58b58982e6fe
isnoopytokky <tokpower2@gmail.com>
2015-03-01 02:01:44 +0700

    fix Facebook login with use keystore

--- app/src/main/java/com/project/thedoc/MainActivity.java
+++ app/src/main/java/com/project/thedoc/MainActivity.java
@@ -1,6 +1,9 @@
 package com.project.thedoc;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -8,6 +11,8 @@ import android.support.v4.app.FragmentManager;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -17,7 +22,12 @@ import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import com.facebook.AppEventsLogger;
 import com.facebook.Session;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class MainActivity extends ActionBarActivity
@@ -37,6 +47,27 @@ public class MainActivity extends ActionBarActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(),
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : packageInfo.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        }
+        catch (PackageManager.NameNotFoundException e1) {
+            Log.e("Name not found", e1.toString());
+        }
+        catch (NoSuchAlgorithmException e) {
+            Log.e("No such an algorithm", e.toString());
+        }
+        catch (Exception e){
+            Log.e("Exception", e.toString());
+        }
+        */
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
@@ -144,6 +175,21 @@ public class MainActivity extends ActionBarActivity
             super.onAttach(activity);
             ((MainActivity) activity).onSectionAttached(
                     getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+        @Override
+        public void onResume() {
+            super.onResume();
+
+            // Logs 'install' and 'app activate' App Events.
+            AppEventsLogger.activateApp(this.getActivity());
+        }
+        @Override
+        public void onPause() {
+
+            super.onPause();
+
+            // Logs 'app deactivate' App Event.
+            AppEventsLogger.deactivateApp(this.getActivity());
         }
     }
 
