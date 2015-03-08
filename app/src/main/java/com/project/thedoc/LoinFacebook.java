3e2fd763f1adc289bdb891c1c9d7bf2bde07f561
isnoopytokky <tokpower2@gmail.com>
2015-03-08 16:51:57 +0700

    fix facebook get fb api

--- app/src/main/java/com/project/thedoc/LoinFacebook.java
+++ app/src/main/java/com/project/thedoc/LoinFacebook.java
@@ -1,18 +1,105 @@
 package com.project.thedoc;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.facebook.AppEventsLogger;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
 
 /**
  * Created by kanokwan on 3/8/15 AD.
  */
+
+
+
 public class LoinFacebook  extends Activity{
+    private static String TAG = LoinFacebook.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_facebook);
 
+    }
+    public static class PlaceholderFragment extends Fragment {
+        private UiLifecycleHelper uiHelper;
+        private Session.StatusCallback callback = new Session.StatusCallback() {
+            @Override
+            public void call(Session session, SessionState state, Exception exception) {
+                onSessionStateChange(session, state, exception);
+            }
+        };
+
+        public PlaceholderFragment() {
+        }
+
+        private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+            if (state.isOpened()) {
+
+                Request request = Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+                    @Override
+                    public void onCompleted(GraphUser user, Response response) {
+                        Toast.makeText(getActivity(),
+                                "Hello " + user.getName(),
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+                request.executeAsync();
+
+            } else if (state.isClosed()) {
+                Log.i(TAG, "Logged out...");
+            }
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+
+            Session session = Session.getActiveSession();
+            if (session != null &&
+                    (session.isOpened() || session.isClosed())) {
+                onSessionStateChange(session, session.getState(), null);
+            }
+
+            uiHelper.onResume();
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            uiHelper.onActivityResult(requestCode, resultCode, data);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            uiHelper.onPause();
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            uiHelper.onDestroy();
+        }
+
+        @Override
+        public void onSaveInstanceState(Bundle outState) {
+            super.onSaveInstanceState(outState);
+            uiHelper.onSaveInstanceState(outState);
+        }
 
     }
 }
