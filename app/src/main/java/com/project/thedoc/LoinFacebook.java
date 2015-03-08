cb08397c7040dab723e7602a6bc0a7475c2a0952
isnoopytokky <tokpower2@gmail.com>
2015-03-08 13:59:06 +0700

    mac change flow

---
+++ app/src/main/java/com/project/thedoc/LoinFacebook.java
@@ -0,0 +1,18 @@
+package com.project.thedoc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+
+/**
+ * Created by kanokwan on 3/8/15 AD.
+ */
+public class LoinFacebook  extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_facebook);
+
+
+    }
+}
