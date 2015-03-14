0a6ede177360d29086a2da6b8c9132354f5138d5
isnoopytokky <tokpower2@gmail.com>
2015-03-15 02:52:46 +0700

    complete toggle button in grid view

---
+++ app/src/main/java/com/project/thedoc/DynamicAdapter.java
@@ -0,0 +1,53 @@
+package com.project.thedoc;
+
+/**
+ * Created by Pariwat on 15/3/2558.
+ */
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import org.askerov.dynamicgrid.BaseDynamicGridAdapter;
+
+import java.util.List;
+
+
+public class DynamicAdapter extends BaseDynamicGridAdapter {
+    public DynamicAdapter(Context context, List<?> items, int columnCount) {
+        super(context, items, columnCount);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item, null);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        holder.build(getItem(position).toString());
+        return convertView;
+    }
+
+    private class ViewHolder {
+        private TextView titleText;
+        private ImageView image;
+        private int Alpha_90 = 90;
+        private ViewHolder(View view) {
+            titleText = (TextView) view.findViewById(R.id.grid_item_title);
+            image = (ImageView) view.findViewById(R.id.grid_item_image);
+        }
+
+        void build(String title) {
+
+            titleText.setText(title);
+            image.setImageResource(R.drawable.icon);
+            //image.setAlpha(Alpha_90);
+        }
+    }
+}
--- app/src/main/java/com/project/thedoc/SickType.java
+++ app/src/main/java/com/project/thedoc/SickType.java
@@ -8,26 +8,61 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.askerov.dynamicgrid.DynamicGridView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Pariwat on 14/3/2558.
  */
 public class SickType extends Fragment {
     View rootview;
+    private ImageView image;
+
     static final String[] MOBILE_OS = new String[] { "Android", "iOS",
             "Windows", "Blackberry" };
 
+    static final String[] Items_List = new String[] {
+            "Case1",
+            "Case2",
+            "Case3",
+            "Case4",
+            "Case5",
+            "Case6",
+            "Case7",
+            "Case8",
+            "Case9"
+    };
+
+    final Map<String, Boolean> Items_Boolean = new HashMap<String, Boolean>();
+    Boolean a = false;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        Items_Boolean.put("Case1", false);
+        Items_Boolean.put("Case2", false);
+        Items_Boolean.put("Case3", false);
+        Items_Boolean.put("Case4", false);
+        Items_Boolean.put("Case5", false);
+        Items_Boolean.put("Case6", false);
+        Items_Boolean.put("Case7", false);
+        Items_Boolean.put("Case8", false);
+        Items_Boolean.put("Case9", false);
+
         rootview = inflater.inflate(R.layout.sick_type, container, false);
 
-        //super.onCreate(savedInstanceState);
-        int iconSize=getResources().getDimensionPixelSize(android.R.dimen.app_icon_size);
-
-        //GridView gridview = (GridView) getActivity().findViewById(R.id.gridView);
         GridView gridview = (GridView) rootview.findViewById(R.id.gridView);
-        gridview.setAdapter(new ImageAdapter(getActivity(), MOBILE_OS));
+        gridview = (DynamicGridView) rootview.findViewById(R.id.dynamic_grid);
+        gridview.setAdapter(new DynamicAdapter(getActivity(),
+                new ArrayList<String>(Arrays.asList(Items_List)),
+                getResources().getInteger(R.integer.column_count)));
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -35,7 +70,33 @@ public class SickType extends Fragment {
             }
         });
 
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int Alpha_100 = 100;
+                int Alpha_90 = 90;
+                Toast.makeText(getActivity(), parent.getAdapter().getItem(position).toString(),
+                        Toast.LENGTH_SHORT).show();
+                image = (ImageView) view.findViewById(R.id.grid_item_image);
+                image.setImageResource(R.drawable.ic_drawer);
 
+                //view.setBackgroundColor(Color.RED);
+                Items_Boolean.put(
+                        parent.getAdapter().getItem(position).toString(),
+                        !Items_Boolean.get(parent.getAdapter().getItem(position).toString())
+                );
+                if(Items_Boolean.get(parent.getAdapter().getItem(position).toString()) == true)
+                {
+                    image.setImageResource(R.drawable.ic_drawer);
+                    //image.setAlpha(Alpha_100);
+                }
+                else
+                {
+                    image.setImageResource(R.drawable.icon);
+                    //image.setAlpha(Alpha_90);
+                }
+            }
+        });
 
         return rootview;
     }
